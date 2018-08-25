8770a7763ce540aa6455fcb155f6addd93e5a3d6
Ashwini-Madke <39327834+Ashwini-Madke@users.noreply.github.com>
2018-08-25 11:45:32 -0700

    file added.

---
+++ GallToLiters.java
@@ -0,0 +1,13 @@
+package com.org;
+
+public class GallToLiters {
+
+	public static void main(String[] args) {
+		double gallons = 10;
+		double liters;
+		liters=gallons*3.7854;
+		System.out.println(gallons +"gallons is  "+ liters +"liters");
+
+	}
+
+}
